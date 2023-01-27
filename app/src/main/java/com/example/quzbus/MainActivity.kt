a7521c16cf9b191efb7f0fecadb42f23ca0a6bfb
xellrzr <revolage.d.chi@gmail.com>
2023-01-27 14:05:57 +0600

    configure project, add dependencies

--- app/src/main/java/com/example/quzbus/MainActivity.kt
+++ app/src/main/java/com/example/quzbus/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.quzbus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+
+var mapView: MapView? = null
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapView = findViewById(R.id.mapView)
+        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
     }
 }
---
+++ app/src/main/java/com/example/quzbus/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.quzbus.utils
+
+class Constants {
+
+    companion object {
+
+        const val MAPBOX_ACCESS_TOKEN = "pk.eyJ1Ijoic25ha2VuZGVhZCIsImEiOiJjbGNvaXBoZ24wOWtoM3FtamJ3NncxbDZoIn0.ZRGrioRJbxVF4bo6ciHh0Q"
+    }
+}
